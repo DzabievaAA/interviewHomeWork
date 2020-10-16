7dc23a0c4ac62170ad0c464c809aba647e150ef9
DzabievaAA <DzabievaAA@gmail.com>
2020-10-16 18:27:59 +0300

    Added styles for TextField and his container

--- components/UserInfoEditor.js
+++ components/UserInfoEditor.js
@@ -4,6 +4,8 @@ import {
 	Button
 } from '@material-ui/core'
 
+import { makeStyles } from '@material-ui/core/styles'
+
 import { useRef, useState } from 'react';
 
 import SaveConfirmDialog from './SaveConfirmDialog';
@@ -20,7 +22,22 @@ const [modalIsOpen, setModalIsOpen] = useState( false );
 
 const refNameSurname = useRef(null);
 const refEmail = useRef(null);
-const refCellphone = useRef(null);										
+const refCellphone = useRef(null);	
+
+const useStyles = makeStyles({
+	textField: {
+		marginLeft: 23,
+		marginRight: 23,
+		marginTop: 26,
+	},
+	containerForFields: {
+		boxShadow: "0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);",
+		marginLeft: 10,
+		marginTop: 10,
+		borderRadius: 10,
+	}
+});
+const classes = useStyles()
 	return (
 		
 	<>
@@ -39,19 +56,20 @@ const refCellphone = useRef(null);
 	/>
 	<Grid container 
 		direction="column"
+		className={classes.containerForFields}
 	>
-		<TextField label="Фамилия и Имя" 
+		<TextField className={classes.textField} label="Фамилия и Имя" 
 			variant="outlined"
 			defaultValue={nameSurname}
 			inputRef={refNameSurname}
 			/>
 
-		<TextField label="E-mail" 
+		<TextField className={classes.textField} label="E-mail" 
 			variant="outlined"
 			defaultValue={email}
 			inputRef={refEmail}/>
 
-		<TextField label="Номер телефона" 
+		<TextField className={classes.textField} label="Номер телефона" 
 			variant="outlined"
 			defaultValue={cellphone}
 			inputRef={refCellphone}/>
