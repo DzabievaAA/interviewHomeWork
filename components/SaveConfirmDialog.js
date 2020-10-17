f65ce004a588ad562f1f2b230f2ca7d7042762c8
DzabievaAA <DzabievaAA@gmail.com>
2020-10-17 17:47:47 +0300

    Adaptive layout fixes for SaveConfirmDialog

--- components/SaveConfirmDialog.js
+++ components/SaveConfirmDialog.js
@@ -7,65 +7,74 @@ import {Grid,
 import { makeStyles } from '@material-ui/core/styles'
 
 import CloseIcon from '@material-ui/icons/Close';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 
 export default function SaveConfirmDialog ( { isOpen, onConfirm, onCancel } ) {
  
-const useStyles = makeStyles( (theme) => ({
-  buttonFull: {
-    backgroundColor: theme.mainColor,
-    width: 212,
-    height: 49,
-    borderRadius: 36,
-    color: "white",
-    marginTop: 26,
-  },
-  buttonEmpty: {
-    width: 212,
-    height: 49,
-    borderRadius: 36,
-    color: theme.mainColor,
-    marginTop: 28,
-    marginBottom: 17,
-    border: `1px solid ${theme.mainColor}`,
-    boxSizing: "border-box",
-  },
-  closeIcon: {
-    marginLeft: "auto",
-    marginTop: 25,
-    marginRight: 25
-  },
-  dialog: {
-    marginTop: 142
-  }
-}));
-const classes = useStyles()
- 
- return (
-<>
-  <Dialog open={isOpen}
-    fullScreen={true}
-    className={classes.dialog}
-  >
-    <CloseIcon onClick={()=>onCancel()} 
-        className={classes.closeIcon}/>
+  const desktop = useMediaQuery('(min-width:600px)'); 
 
-    <Grid container
-      direction="column"
-      alignItems="center"
+  const useStyles = makeStyles( (theme) => ({
+    
+    buttonFull: {
+      backgroundColor: theme.mainColor,
+      width: 212,
+      height: 49,
+      borderRadius: 36,
+      color: "white",
+      marginTop: 26,
+    },
+    buttonEmpty: {
+      width: 212,
+      height: 49,
+      borderRadius: 36,
+      color: theme.mainColor,
+      marginTop: 28,
+      marginBottom: 17,
+      border: `1px solid ${theme.mainColor}`,
+      boxSizing: "border-box",
+    },
+    closeIcon: {
+      marginLeft: "auto",
+      marginTop: 25,
+      marginRight: 25
+    },
+    dialog: {
+      marginTop: 142,
+      [theme.breakpoints.up('sm')]:{
+        marginTop: 0,
+        }
+    }
+  }));
+  const classes = useStyles()
+  
+  return (
+  <>
+    <Dialog open={isOpen}
+      fullScreen={desktop? false : true}
+      maxWidth="sm"
+      fullWidth={true}
+      className={classes.dialog}
     >
-      <DialogTitle>
-        {"Сохранить изменения?"}
-      </DialogTitle>
+      <CloseIcon onClick={()=>onCancel()} 
+          className={classes.closeIcon}/>
 
-      <Button className={classes.buttonFull} onClick={()=>{ onConfirm() }}  color="primary">
-        Сохранить
-      </Button>
+      <Grid container
+        direction="column"
+        alignItems="center"
+      >
+        <DialogTitle>
+          {"Сохранить изменения?"}
+        </DialogTitle>
 
-      <Button className={classes.buttonEmpty} onClick={() => onCancel()} color="primary">
-        Не сохранять
-      </Button>
-    </Grid>
-  </Dialog>
-</>
-    )
+        <Button className={classes.buttonFull} onClick={()=>{ onConfirm() }}  color="primary">
+          Сохранить
+        </Button>
+
+        <Button className={classes.buttonEmpty} onClick={() => onCancel()} color="primary">
+          Не сохранять
+        </Button>
+      </Grid>
+    </Dialog>
+  </>
+      )
 }
