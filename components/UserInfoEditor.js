7875516d6e9406f47557c0fb21dcc4dbc2b79879
DzabievaAA <DzabievaAA@gmail.com>
2020-10-17 16:29:11 +0300

    Adaptive layout fixes for UserInfoEditor component

--- components/UserInfoEditor.js
+++ components/UserInfoEditor.js
@@ -1,7 +1,8 @@
 import {
 	TextField,
 	Grid,
-	Button
+	Button,
+	Hidden
 } from '@material-ui/core'
 
 import { makeStyles } from '@material-ui/core/styles'
@@ -9,6 +10,12 @@ import { makeStyles } from '@material-ui/core/styles'
 import { useRef, useState } from 'react';
 
 import SaveConfirmDialog from './SaveConfirmDialog';
+
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+
+import NameIcon from '@material-ui/icons/AssignmentInd';
+import EmailIcon from '@material-ui/icons/AlternateEmail'
+import PhoneIcon from '@material-ui/icons/Phone'
 
 export default function UserInfoEditor ({nameSurname, 
 										email, 
@@ -28,17 +35,24 @@ const [nameIsValid, setNameIsValid] = useState( true );
 const [emailIsValid, setEmailIsValid] = useState( true );
 const [phoneIsValid, setPhoneIsValid] = useState( true );
 
+const desktop = useMediaQuery('(min-width:600px)');
+
 const useStyles = makeStyles( (theme) => ({
-	textField: {
-		marginLeft: 23,
-		marginRight: 23,
-		marginTop: 26,
-	},
-	containerForFields: {
+	container: {
+		backgroundColor: "white",
 		boxShadow: "0 14px 28px rgba(0,0,0,0.15), 0 10px 10px rgba(0,0,0,0.15);",
 		marginTop: 10,
 		borderRadius: 10,
 		marginRight: 10,
+		[theme.breakpoints.up('sm')]:{
+			marginTop: 24,
+		  }
+	},
+	containerForFields: {
+		marginTop: 10,
+		[theme.breakpoints.up('sm')]:{
+			marginTop: 24,
+		  }
 	},
 	buttonFull: {
 		backgroundColor: theme.mainColor,
@@ -48,6 +62,15 @@ const useStyles = makeStyles( (theme) => ({
 		color: "white",
 		marginTop: 26,
 		marginBottom: 17,
+	},
+	icon:{
+		fill: theme.mainColor,
+		width:20,
+		height:20,
+		[theme.breakpoints.up('sm')]:{
+			width: 30,
+			height: 30,
+		  }
 	}
 }));
 const classes = useStyles()
@@ -77,36 +100,80 @@ const classes = useStyles()
 			setEditing( false );
 		}}
 	/>
+
 	<Grid container 
 		direction="column"
 		alignItems="center"
-		className={classes.containerForFields}
+		className={classes.container}
 	>
-		<TextField className={classes.textField} label="Фамилия и Имя" 
-			variant="outlined"
-			defaultValue={nameSurname}
-			inputRef={refNameSurname}
-			error={!nameIsValid}
-			onChange={( event ) => setNameIsValid( validateName(event.target.value))}
-			/>
+		<Grid container 
+			direction= {desktop? "row": "column"}
+			justify="space-evenly"
+			alignItems="center"
+			className={classes.containerForFields}
+			xs={12}
+			spacing={2}
+		>
+			{
+				["Фамилия и Имя", null, "E-mail", null, "Номер телефона"].map( label => {
+					if( label === null){
+						return(
+							<Hidden smDown>
+								<svg width="1" height="97" viewBox="0 0 1 97" fill="none" xmlns="http://www.w3.org/2000/svg">
+									<line x1="0.5" y1="2.18557e-08" x2="0.499996" y2="97" stroke="#CAE7FE"/>
+								</svg>
+							</Hidden>
+						);
+					}
 
-		<TextField className={classes.textField} label="E-mail" 
-			variant="outlined"
-			defaultValue={email}
-			inputRef={refEmail}
-			error={!emailIsValid}
-			onChange={( event ) => setEmailIsValid( validateEmail(event.target.value))}
-			/>
-			
+					let icon, defaultValue, inputRef, error, onChange;
+					switch( label ){
+						case 'Фамилия и Имя':
+							icon = <NameIcon className={classes.icon}/>
+							defaultValue = nameSurname
+							inputRef = refNameSurname
+							error = !nameIsValid
+							onChange = ( event ) => setNameIsValid( validateName(event.target.value))
+							break;
+						case 'E-mail':
+							icon = <EmailIcon className={classes.icon}/>
+							defaultValue = email
+							inputRef = refEmail
+							error = !emailIsValid
+							onChange = ( event ) => setEmailIsValid( validateEmail(event.target.value))
+							break;
+						case 'Номер телефона':
+							icon = <PhoneIcon className={classes.icon}/>
+							defaultValue = cellphone
+							inputRef = refCellphone
+							error = !phoneIsValid
+							onChange = ( event ) => setPhoneIsValid( validatePhone(event.target.value))
+							break;
+					}
 
-		<TextField className={classes.textField} label="Номер телефона" 
-			variant="outlined"
-			defaultValue={cellphone}
-			inputRef={refCellphone}
-			error={!phoneIsValid}
-			onChange={( event ) => setPhoneIsValid( validatePhone(event.target.value))}
-			/>
-
+					return(
+					<Grid item xs={10} sm={3}>
+						<Grid container alignItems="center" direction="row">
+							<Grid item sm={2}>
+								<Hidden smDown>
+									{icon}
+								</Hidden>
+							</Grid>
+							<Grid item xs={12} sm={10}>
+								<TextField className={classes.textField} label={label} 
+									variant="outlined"
+									defaultValue={defaultValue}
+									inputRef={inputRef}
+									error={error}
+									onChange={onChange}
+									/>
+								</Grid>
+						</Grid>
+					</Grid>
+					)
+				})
+			}
+		</Grid>
 		<Button className={classes.buttonFull} onClick={() => setModalIsOpen(true)}> Сохранить изменения </Button>
 	</Grid>
 	</>
